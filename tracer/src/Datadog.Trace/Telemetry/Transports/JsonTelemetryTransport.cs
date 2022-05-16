0a98e4cef1d6680a364ea64f3418d6c7e5ad9ff4
Andrew Lock <andrew.lock@datadoghq.com>
2022-05-16 15:18:59 +0100

    Removed agent version check from AgentlessTelemetryTransport
    The agent doesn't send the AgentVersion header if Telemetry is not supported (404) which makes this check a bit pointless (we will only receive the header if Telemetry _is_ supported)
    
    We _could_ look at reading the agent version from the tracer `API`, but it all seems a little pointless
    
    Removed TelemetryPushResult.FatalErrorDontRetry code paths as they will never be hit now

--- tracer/src/Datadog.Trace/Telemetry/Transports/JsonTelemetryTransport.cs
+++ tracer/src/Datadog.Trace/Telemetry/Transports/JsonTelemetryTransport.cs
@@ -53,11 +53,6 @@ namespace Datadog.Trace.Telemetry.Transports
                     return TelemetryPushResult.Success;
                 }
 
-                if (HandleErrorResponse(response) is { } result)
-                {
-                    return result;
-                }
-
                 if (response.StatusCode == 404)
                 {
                     Log.Debug("Error sending telemetry: 404. Disabling further telemetry, as endpoint '{Endpoint}' not found", GetEndpointInfo());
@@ -97,7 +92,5 @@ namespace Datadog.Trace.Telemetry.Transports
                        or WebException { Response: HttpWebResponse { StatusCode: HttpStatusCode.NotFound } }
                        or WebException { InnerException: SocketException };
         }
-
-        protected abstract TelemetryPushResult? HandleErrorResponse(IApiResponse apiResponse);
     }
 }
