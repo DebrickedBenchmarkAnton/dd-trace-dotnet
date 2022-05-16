d0a72db79844d1c111e72d36b10da49ebd69f0c9
Andrew Lock <andrew.lock@datadoghq.com>
2022-05-16 10:16:32 +0100

    Add additional logging

--- tracer/test/Datadog.Trace.Tests/Telemetry/TelemetryControllerTests.cs
+++ tracer/test/Datadog.Trace.Tests/Telemetry/TelemetryControllerTests.cs
@@ -183,6 +183,8 @@ namespace Datadog.Trace.Tests.Telemetry
                 _data.Push(data);
                 return Task.FromResult(_pushResult);
             }
+
+            public string GetTransportInfo() => nameof(TestTelemetryTransport);
         }
     }
 }
